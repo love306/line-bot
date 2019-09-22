f610d5fcbff84a2e5a51f4893ce98759942d0bc7
love306 <j127289j@gmail.com>
2019-09-22 21:19:36 +0800

    add msg

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
             sticker_message)
 
         
-    if msg in ['哈囉', 'hi', 'Hi']:
+    if msg == '哈囉':
         s = '哈囉模投'
     elif msg in ['你是誰', '你是']:
         s = '大家好，我是人工智障機器人'
