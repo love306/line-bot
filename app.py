d08d52dc498a1ebd46504945541457a904ae2a88
love306 <j127289j@gmail.com>
2019-09-22 22:09:43 +0800

    add new sticker

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
+import random
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('QPbdjZugpGl0WpGXy3b5QS2n5CYXPIGL4SZueo6eCYDFOxcY9UVg28kwKDmdJe7UlfYlvuqnYYVhRCLRSY6BwLanxHqwXyJ9aZPRMBUv1j9FTJkckcT43w2iTIB9BrJysrBLaxOnXnfl3t1N6PpBKAdB04t89/1O/w1cDnyilFU=')
@@ -38,12 +40,19 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '母鯊大，公鯊?'
+    r = random.randint(11537, 11539)
+    if r == 11537:
+        rr = random.randint(52002734, 52002733)
+    elif r == 11538:
+        rr = random.randint(51626494, 51626533)
+    elif r == 11539:
+        rr = random.randint(52114210, 52114149)
+
 
     if msg in ['給我貼圖', '貼圖', 'sticker']:
         sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
+            package_id=r,
+            sticker_id=rr
         )
         line_bot_api.reply_message(
             event.reply_token,
@@ -54,6 +63,8 @@ def handle_message(event):
         s = '哈囉模投'
     elif msg in ['你是誰', '你是']:
         s = '大家好，我是人工智障機器人'
+    else:
+        s = '母鯊大，公鯊?'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s
